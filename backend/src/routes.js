1e5c8572ff4dfcb1cc45db95a778e2b73f38cc50
Pedro Bueno <pedro@mentores.com.br>
2019-12-19 18:45:28 -0300

    CRUD de estudantes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import authMiddleware from './app/middlewares/auth';
 import SessionController from './app/controllers/SessionController';
+import StudentController from './app/controllers/StudentController';
 
 const routes = new Router();
 
@@ -9,5 +10,10 @@ routes.get('/', (req, res) => res.json('Hello GymPoint'));
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
+
+routes.get('/students', StudentController.index);
+routes.get('/students/:id', StudentController.show);
+routes.post('/students', StudentController.store);
+routes.put('/students/:id', StudentController.update);
 
 export default routes;
